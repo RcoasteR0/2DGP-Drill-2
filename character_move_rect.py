227806fb638f0de2f12996b5cea971d5fe192296
RcoasteR0 <st_anthony@naver.com>
2024-09-23 12:18:24 +0900

    Drill 2 제출 (2022180007 김용채)

---
+++ character_move_rect.py
@@ -0,0 +1,37 @@
+from pico2d import *
+import os
+import math
+
+os.chdir('C:\\Git\\2DGP\\2DGP-Drill-2')
+
+open_canvas()
+
+grass = load_image('grass.png')
+character = load_image('character.png')
+
+x = 0
+movex = 5
+y = 90
+movey = 0
+
+
+while (True):
+    clear_canvas_now()
+    grass.draw_now(400,30)
+    character.draw_now(x, y)
+    x = x + movex
+    y = y + movey
+    if x >= 800 and y <= 90:
+        movex = 0
+        movey = 5
+    elif x >= 800 and y >= 600:
+        movex = -5
+        movey = 0
+    elif x <= 0 and y >= 600:
+        movex = 0
+        movey = -5
+    elif x <= 0 and y <= 90:
+        movex = 5
+        movey = 0
+    delay(0.01)
+
